f4f297a339b3ff69f26abab5fa26a7a40ccf347c
Luizin0404 <161052394+Luizin0404@users.noreply.github.com>
2024-02-23 12:33:09 -0300

    Add files via upload

---
+++ trabalho/js/disciplinas.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $("#elemento1").click(function(){
+        $(".Texto1").hide(200);
+        $("#conteudo1").show(20);
+    });
+    $("#elemento2").click(function(){
+        $(".Texto1").hide(200);
+        $("#conteudo2").show(20);
+    });
+    $("#elemento3").click(function(){
+        $(".Texto1").hide(200);
+        $("#conteudo3").show(20);
+    });
+
+    $("#elemento4").click(function(){
+        $(".Texto1").hide(200);
+        $("#conteudo4").show(20);
+    });
+
+});
